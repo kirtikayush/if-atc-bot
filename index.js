fb7d172a8248b388bdd340ef5581538eb9e57d4e
kirtikayush <kirtikayush2000@gmail.com>
2026-06-05 23:36:31 +0530

    fixed total

--- index.js
+++ index.js
@@ -350,9 +350,10 @@ client.on("interactionCreate", async (interaction) => {
 
           if (mode === "total") {
             return (
-              `**${i + 1}. ${a.icao}** ${atc}\n` +
-              `📥 Inbound: ${a.inbound}\n` +
-              `📤 Outbound: ${a.outbound}`
+              `**${i + 1} ${a.icao} - ${atc}\n**` +
+              `20m: ${String(a.next20).padStart(3)} | ` +
+              `60m: ${String(a.next60).padStart(3)} | ` +
+              `Total: ${String(a.inbound).padStart(3)}`
             );
           }
 
